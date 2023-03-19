5e67772330f61ad8641f72def45258ea5dc9413d
André Felipe Dias <andre.dias@pronus.io>
2023-03-19 13:53:05 -0300

    more performant query for allowed repositories

--- gitclub/models/repository.py
+++ gitclub/models/repository.py
@@ -1,5 +1,7 @@
+from collections.abc import Sequence
+
 from pydantic import BaseModel
-from sqlalchemy import Column, ForeignKey, Integer, String, Table, UniqueConstraint
+from sqlalchemy import Column, ForeignKey, Integer, String, Table, UniqueConstraint, bindparam, text
 
 from ..resources import db
 from . import metadata, random_id
@@ -41,6 +43,43 @@ async def get(id: int, organization_id: int | None = None) -> RepositoryInfo | N
         query = query.where(Repository.c.organization_id == organization_id)
     result = await db.fetch_one(query)
     return RepositoryInfo(**result._mapping) if result else None
+
+
+async def get_allowed_repositories(
+    user_id: int,
+    org_roles: Sequence[str],
+    repo_roles: Sequence[str],
+) -> list[RepositoryInfo]:
+    query = text(
+        """\
+    select
+        r.id, r.name, r.organization_id
+    from
+        repository r
+    inner join
+        user_repository ur
+    on
+        r.id = ur.repository_id
+        and ur.user_id = :user_id
+        and ur.role in :repo_roles
+union
+    select
+        r.id, r.name, r.organization_id
+    from
+        repository r
+    inner join
+        user_organization uo
+    on
+        r.organization_id = uo.organization_id
+        and uo.user_id = :user_id
+        and uo.role in :org_roles"""
+    ).bindparams(
+        bindparam('repo_roles', value=repo_roles, expanding=True),
+        bindparam('org_roles', value=org_roles, expanding=True),
+        user_id=user_id,
+    )
+    result = await db.fetch_all(query)
+    return [RepositoryInfo(**row._mapping) for row in result]
 
 
 async def get_organization_repositories(organization_id: int) -> list[RepositoryInfo]:
